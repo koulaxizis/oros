7e6ffb2b1faee2a0469b2262cc455b5b89149564
Christos Koulaxizis <koulaxizis@glarolykoi.net>
2026-09-11 20:56:45 +0300

    Update shell.js

--- shell.js
+++ shell.js
@@ -24,7 +24,7 @@
 (function () {
   "use strict";
 
-  var APP_VERSION = "0.20.0";   // bump on every deploy (shows welcome toast)
+  var APP_VERSION = "0.23.0";   // bump on every deploy (shows welcome toast)
   var VERSION_KEY = "oros-last-version";
 
   // ---------- 1. State & registries ----------
